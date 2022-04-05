aaf1b2ee604acb625d701a48a91daeaa981a0842
muhammadabdurrohim574@gmail.com <muhammadabdurrohim574@gmail.com>
2022-04-05 20:49:34 +0700

    commit type data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,49 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_pertama_1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+//tipe data dan variable
+//var
+  var mahasiswa = "Rohim";
+  var umur = "22";
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  print(mahasiswa + "Umur = " + umur);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//string
+  String mahasiswaString = "sisteminformasi";
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  print(mahasiswaString);
+
+
+//int
+int semester;
+semester = 6;
+
+print(semester);
+
+//double
+double ipk;
+ipk = 3.9;
+
+print(ipk);
+
+//boolean
+bool benar = true;
+bool salah = false;
+bool tidakbenar = true;
+bool tidaksalah = false;
+
+//List
+List jurusan = ["Sistem Komputerisasi Akuntansi", "Sistem Informasi Enterprise", semester.toString(), ipk.toString()];
+
+print(jurusan);
+
+//Map
+Map kelas = {
+  "nama": "Muhammad",
+  "kelas": "SIKA 6"
+};
+
+print(kelas);
+print(kelas['nama']);
+print(kelas['kelas']);
 }
